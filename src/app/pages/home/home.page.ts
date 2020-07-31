66c98829c68eda64bd6947bedb271c452895d58a
Jose Basanta <josedavidb6@gmail.com>
2020-07-31 03:53:18 -0400

    Fix: Adjust Watson window chat size

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { WatsonService } from 'src/app/services/watson.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 declare var window: any;
 @Component({
@@ -15,9 +16,11 @@ export class HomePage implements OnInit {
   constructor(
     public _authService:AuthService,
     public _watsonService:WatsonService,
+    public _loadingService:LoadingService,
   ) { }
 
   ngOnInit() {
+    this._loadingService.showLoader('Cargando');
     this.user = this._authService.getUserPersonalInfo();
 
     //Watson Assistance
@@ -27,8 +30,22 @@ export class HomePage implements OnInit {
       integrationID: "bbcc33de-33ab-4c46-8dac-49840a25ca71", // The ID of this integration.
       region: "us-south", // The region your integration is hosted in.
       serviceInstanceID: "89f5b526-f6cd-46cd-a3f1-ece45ac44fe1", // The ID of your service instance.
-      onLoad: function(instance) { retriveInstance(instance); instance.render();}
+      onLoad: function(instance) { retriveInstance(instance);
+        instance.on({ type: "window:open", handler: handler });
+        instance.render().then(()=>{
+        })
+      },
+      disableCustomElementMobileEnhancements: true,
     };
+
+    var handler = function(event){
+      setTimeout(()=>{
+        var element:any = <HTMLElement>document.getElementsByClassName("WACBotContainer").item(0) as HTMLElement
+        element.style = 'width: 80%; margin-left: 20px;height: 70%; margin-top: 40px'
+      },200)
+    }
+
+
     
     setTimeout(function(){
       const t=document.createElement('script');
@@ -38,7 +55,14 @@ export class HomePage implements OnInit {
     });
     setTimeout(()=>{
       this.watson = watson_ngOn;
-      console.log(this.watson)
+      if (this.watson){
+        this._loadingService.hideLoader()
+      }else{
+        setTimeout(()=>{
+          this.watson = watson_ngOn
+          this._loadingService.hideLoader()
+        },2000)
+      }
       this._watsonService.setInstance(this.watson)
     },4000)
   }
